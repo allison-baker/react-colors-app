bc1c8757aae47b4458dcc716b9d4e20d2284cd97
allison-baker <baker.allison.m@gmail.com>
2024-04-15 21:25:28 -0600

    add logo and match favicon

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { Outlet } from 'react-router-dom'
 function App() {
 	return (
 		<>
-			<nav className='bg-base-200 flex flex-row items-center justify-between'>
+			<nav className='flex flex-row items-center justify-between'>
 				<a href='/'>
 					<div className='flex gap-4 items-center ml-4'>
-						<img src='https://placehold.co/75x75' alt='logo placeholder' width='75' height='75' />
+						<img src='/logo1.svg' alt='logo placeholder' width='75' height='75' />
 						<h1 className='text-3xl'>Paletter</h1>
 					</div>
 				</a>
@@ -15,7 +15,7 @@ function App() {
 					<li>
 						<a
 							href='/search/colors'
-							className='block px-8 py-10 uppercase tracking-widest font-bold hover:text-neutral hover:bg-base-300 text-primary'
+							className='block px-8 py-10 uppercase tracking-widest font-bold hover:text-primary-content hover:bg-primary text-primary hover:rounded-full'
 						>
 							Colors
 						</a>
@@ -23,7 +23,7 @@ function App() {
 					<li>
 						<a
 							href='/search/palettes'
-							className='block px-8 py-10 uppercase tracking-widest font-bold hover:text-neutral hover:bg-base-300 text-secondary'
+							className='block px-8 py-10 uppercase tracking-widest font-bold hover:text-secondary-content hover:bg-secondary text-secondary hover:rounded-full'
 						>
 							Palettes
 						</a>
@@ -31,13 +31,16 @@ function App() {
 					<li>
 						<a
 							href='/search/patterns'
-							className='block px-8 py-10 uppercase tracking-widest font-bold hover:text-neutral hover:bg-base-300 text-success'
+							className='block px-8 py-10 uppercase tracking-widest font-bold hover:text-success-content hover:bg-success text-success hover:rounded-full'
 						>
 							Patterns
 						</a>
 					</li>
 					<li>
-						<a href='/favorites' className='block px-8 py-10 uppercase tracking-widest font-bold hover:text-neutral hover:bg-base-300 text-warning'>
+						<a
+							href='/favorites'
+							className='block px-8 py-10 uppercase tracking-widest font-bold hover:text-warning-content hover:bg-warning text-warning hover:rounded-full'
+						>
 							Favorites
 						</a>
 					</li>
